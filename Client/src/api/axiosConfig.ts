00371bbdfffb2da4058718692771fa5830306f94
AdonaiPadilla <2236000388@utna.edu.mx>
2026-08-11 15:33:51 -0600

    proyectosubido a la nube no cargan los libros

--- Client/src/api/axiosConfig.ts
+++ Client/src/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.137.1:3000/api';
+const API_URL = 'https://api-libros-j8v0.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
